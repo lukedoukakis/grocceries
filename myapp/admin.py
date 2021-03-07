baf83323021a30c0f09e823386877e07b626776b
Luke Doukakis <lukedoukakis97@gmail.com>
2021-03-06 16:18:46 -0800

    db resync

--- myapp/admin.py
+++ myapp/admin.py
@@ -4,12 +4,10 @@ from .models import *
 # Register your models here.
 # allows models to be viewed and edited from admin page
 
-admin.site.register(Test)
-admin.site.register(ForeignKeyExample)
-admin.site.register(AddressTable)
-admin.site.register(Account)
+admin.site.register(Address)
 admin.site.register(Item)
 admin.site.register(Nutrition)
 admin.site.register(ShoppingCart)
 admin.site.register(Order)
+admin.site.register(Account)
 admin.site.register(Vendor)
--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,19 @@ from django.db import models
 
 
 # ------------------------------------------------------
+# ~~~ FUNCTIONS ~~~
+
+# definition for adding an item
+# def add_item(_itemID, _name, _price):
+#     item = Item(itemID=_itemID, name=_name, price=_price)
+#     item.save()
+
+
+
+
+# ------------------------------------------------------
+# ~~~ TABLES ~~~
+
 # NOTES FOR USING THE TABLES IN A PYTHON SCRIPT OR SHELL
 
 # To add via shell:
@@ -20,111 +33,61 @@ from django.db import models
 
 # get all results in a database (returns QuerySet):
 # <ModelName>.objects.all()
-# ------------------------------------------------------
-
-# definition for adding an item
-def add_item(_itemID, _name, _price):
-    item = Item(itemID=_itemID, name=_name, price=_price)
-    item.save()
 
 
 
-class AddressTable(models.Model):
-    email = models.CharField(max_length=255)        # pk
-    address = models.CharField(max_length=255)
+class Address(models.Model):
+    value = models.CharField(max_length=255)
 
-    # set id to email
     def __str__(self):
-        return self.email
+        return self.value
 
 
 class Item(models.Model):
-    itemID = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.itemID
+        return self.name
 
 
 class Nutrition(models.Model):
-    itemID = models.OneToOneField(Item, on_delete=models.CASCADE)
+    item = models.OneToOneField(Item, on_delete=models.CASCADE)
     calories = models.CharField(max_length=255)
     sodium = models.CharField(max_length=255)
     fat = models.CharField(max_length=255)
-    Carbs = models.CharField(max_length=255)
-    Protein = models.CharField(max_length=255)
-
-    # set id to id
-
-    def __str__(self):
-        return self.itemID
+    carbs = models.CharField(max_length=255)
+    protein = models.CharField(max_length=255)
 
 
 class ShoppingCart(models.Model):
-    cartNumber = models.CharField(max_length=255)
-    # an item can have many shopping carts and a shopping cart can have many items
-    items = models.ManyToManyField(Item, blank=True)
-
-    def __str__(self):
-        return self.cartNumber  # set id to cartNumber
+    items = models.ForeignKey(Item, on_delete=models.CASCADE)
 
 
 class Order(models.Model):
-    id = models.CharField(max_length=255, primary_key=True)
     driver = models.CharField(max_length=255)
 
 
 class Account(models.Model):
-    email = models.CharField(max_length=255, default=False)        # pk
-    name = models.CharField(max_length=255, default=False)
-    phone = models.CharField(max_length=255, default=False)
-    primaryAddress = models.CharField(max_length=255, default=False)
-    shoppingCart = models.OneToOneField(
-        ShoppingCart,
-        on_delete=models.CASCADE,
-        default=False
-    )
-    addressTable = models.ManyToManyField(AddressTable, default=False)
+    username = models.CharField(max_length=255, default='default')
+    email = models.CharField(max_length=255, default='default')
+    firstName = models.CharField(max_length=255, default='default')
+    lastName = models.CharField(max_length=255, default='default')
+    phone = models.CharField(max_length=255, default='default')
+    shoppingCart = models.OneToOneField(ShoppingCart, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
     is_driver = models.BooleanField(default=False)
     # one to many//an account can have many orders
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, default=False)
-
-    # set id to email
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.email
+        return self.username
 
 
 class Vendor(models.Model):
-    id = models.CharField(max_length=255, primary_key=True)
-    storeName = models.CharField(max_length=255)
-    storeLocation = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
     items = models.ForeignKey(Item, on_delete=models.CASCADE)
-  # set id to id
-
-    def __str__(self):
-        return self.id
-
-
-# represents a table
-class Test(models.Model):
-
-    # fields/columns for the table
-    text = models.CharField(max_length=10)
-    number = models.IntegerField(null=True)
-    url = models.URLField(default='www.example.com')
-
-    # specifies that the id of an entry will be its number (optional)
-    def __str__(self):
-        return self.text
-
-
-# this one takes an entry from another table as a field
-class ForeignKeyExample(models.Model):
-    name = models.CharField(max_length=10)
-    number = models.IntegerField(null=True)
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
