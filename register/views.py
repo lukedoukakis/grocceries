d46c4332289170b4b9134a082039aae189906df0
Osman Halwani <osmanhalwani@gmail.com>
2021-03-18 23:34:57 -0700

    added if username is taken it will tell the user also when user registers it will log them in automattically if it is successful

--- register/views.py
+++ register/views.py
@@ -3,25 +3,35 @@ from .forms import RegisterForm
 from account.models import Account
 from django.db import IntegrityError
 from django.contrib.auth import authenticate , login
-
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 # used for register page
 def register(response):
+    
     try:
         if response.method == "POST":
             form = RegisterForm(response.POST)
             if form.is_valid():
-                user = form.save()
-                account = Account(
-                    email=user.email, user=user, firstName=user.first_name, lastName=user.last_name)
-                account.save()
-                login(response,user)
-                return render(response, 'myapp/landingPage.html')
-            # after logging in add redirect page here
+                userEmail = form.cleaned_data['email']
+                print(userEmail)
+                if User.objects.filter(email = userEmail).exists(): # if email already exisits in database for users
+                    return render(response,"account/registered.html",{"form":form , "emailPresent":True} , )
+                    
+                else: # if email is unique go here 
+                    user = form.save()
+        
+                    account = Account(
+                            email=user.email, user=user, firstName=user.first_name, lastName=user.last_name)
+                    account.save()
+                    
+                    login(response,user)
+                    return render(response, 'myapp/landingPage.html')
+                # after logging in add redirect page here
 
     except IntegrityError:
-        #add error message your username/email/phone is already taken use a different one please
-        return render(response, 'myapp/landingPage.html')
+            
+            return render(response, 'myapp/landingPage.html')
 
 
     form = RegisterForm()
